9bb37519673388c889cbab0c67168860ba68ce45
pawelpyc154@gmail.com <pawelpyc154@gmail.comnpm>
2021-03-06 11:41:58 +0100

    add eslint prettier to backend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 
 // const dotenv = require("dotenv");
 // const cookieParser = require("cookie-parser");
@@ -8,15 +8,15 @@ const express = require("express");
 // const errorHandler = require("./middleware/error");
 // modules
 // const protect = require("./modules/protect");
-const routes = require("./modules/routes");
+const routes = require('./modules/routes');
 // const connectDB = require("./modules/db");
-const devHeplers = require("./modules/devHelpers");
+const devHeplers = require('./modules/devHelpers');
 
 // dotenv.config({path: "./config/config.env"});
 
 // connectDB();
 
-let app = express();
+const app = express();
 
 // app.use(express.json()); // Json parser
 // app.use(cookieParser()); // Cookie parser
@@ -31,7 +31,4 @@ routes(app);
 // app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
-  PORT,
-  console.log(`Server running on port ${PORT}  http://localhost:${PORT}/api`.yellow.bold)
-);
+app.listen(PORT, console.log(`Server running on port ${PORT}  http://localhost:${PORT}/api`.yellow.bold));
