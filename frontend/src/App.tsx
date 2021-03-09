a01b1032b335b9518e289fff8049b50e0cf0019a
pawelpyc154@gmail.com <pawelpyc154@gmail.comnpm>
2021-03-09 20:26:55 +0100

    remove fetch me form app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import { useQuery } from 'react-query';
 import Navigation from './components/navigation';
 import Pages from './pages';
-import axiosApi from './utils/axiosApi';
 
 export interface AppProps {}
 
-const getMe = () => axiosApi.get('/auth/me').then((res) => res.data.data);
-
-const App: React.FC<AppProps> = () => {
-  const { data } = useQuery('user', getMe, {
-    retry: 1,
-  });
-
-  return (
-    <>
-      <Navigation email={data?.email} />
-      {JSON.stringify(data)}
-      <Pages />
-    </>
-  );
-};
+const App: React.FC<AppProps> = () => (
+  <>
+    <Navigation />
+    <Pages />
+  </>
+);
 export default App;
